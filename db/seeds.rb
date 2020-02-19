ccc27545b79038a5056e6d30af13ca1f48acbf02
Coline Bricteux <coline.bricteux@gmail.com>
2020-02-19 14:29:38 +0100

    on the way

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+restaurants = Restaurant.create([
+  { name: 'Nenu', address: 'Louise', category: 'japanese' },
+  { name: 'Old Boy', address: 'Chatelain', category: 'chinese' },
+  { name: 'Certo', address: 'Saint Boniface', category: 'italian' },
+  { name: 'Gazzetta', address: 'Stephanie', category: 'italian' },
+  { name: 'Saco', address: 'Schaerbeek', category: 'italian' }
+  ])
+
+# Review.create(content: 'trop bon', restaurant: movies.first)
